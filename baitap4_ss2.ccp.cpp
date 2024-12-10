d8de6093bfa2d06aeaf9e56d498e1cc34c74dd8b
MaiQuybinh <maiquybinh2104@gmail.com>
2024-12-10 20:12:40 +0700

    Add files via upload

---
+++ baitap4_ss2.ccp.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+
+int side = 5;
+int perimeter = 4 * side;
+int area = side * side;
+
+
+    printf("The side of the square is: %d\n", side);
+    printf("The perimeter of the square is: %d\n", perimeter);
+    printf("The area of the square is: %d\n", area);
+
+
+
+    
+   
+  return 0;
+}
